0431d9c7127a26a430a048d096408a924612a562
Donggeun Lee <34597321+code-Pat@users.noreply.github.com>
2021-09-29 15:48:52 +0900

    MVP Design Pattern
    MVP Design Pattern practice

---
+++ MVPExample/MVPExample/MVP/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MVPExample
+//
+//  Created by Donggeun Lee on 2021/09/29.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let email: String
+    let username: String
+    
+}
